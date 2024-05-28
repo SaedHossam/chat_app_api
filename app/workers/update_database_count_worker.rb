2a75ba9d0453028d199a2459d5b3e00faab94d57
Saed Hossam <said.hossam12@gmail.com>
2024-05-28 21:04:27 +0300

    Removed unused workers

--- app/workers/update_database_count_worker.rb
+++ app/workers/update_database_count_worker.rb
@@ -1,18 +1,12 @@
 class UpdateDatabaseCountWorker
   include Sidekiq::Worker
 
-
-  def perform(*args)
-    puts 'Starting count job'
-  
+  def perform(*args) 
     # Process 'chats_count'
-    puts $redis.smembers('chat_count')
     app_tokens = $redis.smembers('chat_count')
-    
     app_tokens.each do |token|
       $redis.watch(token) do
         curr = Application.find_by(token: token)
-        
         if curr
           stored_value = $redis.get(token).to_i
           new_value = (curr.chats_count || 0)  + stored_value
@@ -21,20 +15,15 @@ class UpdateDatabaseCountWorker
             curr.update(chats_count: new_value)
           end
         end
-  
         $redis.srem('chat_count', token)
       end
     end
   
     # Process 'messages_count'
-    puts 'Updating messages'
-    puts $redis.smembers('messages_count')
     chat_ids = $redis.smembers('messages_count')
-  
     chat_ids.each do |chat_id|
       $redis.watch(chat_id) do
         curr_chat = Chat.find_by(id: chat_id)
-  
         if curr_chat  
           stored_value = $redis.get(chat_id).to_i
           new_value = (curr_chat.messages_count || 0)  + stored_value
@@ -43,10 +32,8 @@ class UpdateDatabaseCountWorker
             curr_chat.update(messages_count: new_value)
           end
         end
-  
         $redis.srem('messages_count', chat_id)
       end
     end
   end
-  
 end
